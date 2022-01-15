317a5de3f64a5f5c7ab4cd4d1b4303ec2c6eb302
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-01-14 20:02:15 -0500

    lifecycle handler examples

--- example/src/donut/frontend/example/frontend_routes.cljs
+++ example/src/donut/frontend/example/frontend_routes.cljs
@@ -34,13 +34,14 @@
 
    ["/nav.flow/1"
     {:name       :nav.flow-1
-     :lifecycle  {:param-change (fn [_ _ {:keys [params]}])}
+     :lifecycle  {:param-change [::denfc/inc-flow-1-lifecycle-fire-count]}
      :components {:main [denfc/examples]}
      :title      "Donut Examples"}]
 
    ["/nav.flow/2"
     {:name       :nav.flow-2
-     :lifecycle  {:param-change (fn [_ _ {:keys [params]}])}
+     :lifecycle  {:param-change (fn [_ _ {:keys [params]}]
+                                  [::denfc/set-flow-2-val (str (random-uuid))])}
      :components {:main [denfc/examples]}
      :title      "Donut Examples"}]
    ])
--- example/src/donut/frontend/example/nav/flow_component.cljs
+++ example/src/donut/frontend/example/nav/flow_component.cljs
@@ -1,5 +1,6 @@
 (ns donut.frontend.example.nav.flow-component
   (:require
+   [donut.frontend.core.flow :as dcf]
    [donut.frontend.nav.components :as dnc]
    [donut.frontend.nav.flow :as dnf]
    [re-frame.core :as rf]))
@@ -19,6 +20,25 @@
    [route-sub ::dnf/nav-state]
    [route-sub ::dnf/route-name]])
 
+(rf/reg-event-db ::inc-flow-1-lifecycle-fire-count
+  [rf/trim-v]
+  (fn [db _]
+    (update db ::flow-1-val (fnil inc 0))))
+
+(rf/reg-event-db ::set-flow-2-val
+  [rf/trim-v]
+  (fn [db [val]]
+    (assoc db ::flow-2-val val)))
+
+(defn lifecycle-handlers
+  []
+  [:div
+   [:h3 "lifecycle handlers"]
+   [:div ":nav.flow-1 lifecycle fire count:"
+    @(rf/subscribe [::dcf/get-in [::flow-1-val]])]
+   [:div ":nav.flow-2 lifecycle fire count:"
+    @(rf/subscribe [::dcf/get-in [::flow-2-val]])]])
+
 (defn links
   []
   [:div
@@ -33,4 +53,5 @@
   []
   [:div
    [links]
-   [route-subs]])
+   [route-subs]
+   [lifecycle-handlers]])
