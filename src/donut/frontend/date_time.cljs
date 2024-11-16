8b0c9c00d30c60de138bee5a2386fbc13b743d91
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2024-11-16 07:33:20 -0500

    relative time formatting

---
+++ src/donut/frontend/date_time.cljs
@@ -0,0 +1,24 @@
+(ns donut.frontend.date-time)
+
+(def relative-time-format
+  (Intl/RelativeTimeFormat.))
+
+(defn time-ago
+  ([datetime]
+   (time-ago datetime relative-time-format))
+  ([datetime rtf]
+   (let [diff    (/ (- (.getTime (js/Date.))
+                       (.getTime datetime))
+                    1000)
+         minutes (Math/floor (/ diff 60))
+         hours   (Math/floor (/ minutes 60))
+         days    (Math/floor (/ hours 24))
+         months  (Math/floor (/ days 30))
+         years   (Math/floor (/ months 12))]
+     (cond
+       (> years 0)   (.format rtf (* -1 years) "years")
+       (> months 0)  (.format rtf (* -1 months) "months")
+       (> days 0)    (.format rtf (* -1 days) "days")
+       (> hours 0)   (.format rtf (* -1 hours) "hours")
+       (> minutes 0) (.format rtf (* -1 minutes) "minutes")
+       :else         (.format rtf (* -1 diff) "seconds")))))
