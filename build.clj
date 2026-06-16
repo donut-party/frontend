813637b8b7ef0f4dc4e12deb5f5130a2817d9dc4
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-06-16 18:57:39 -0400

    build update

--- build.clj
+++ build.clj
@@ -19,7 +19,7 @@
 
 (defn- pom-template [version]
   [[:description "dependency injection lib / framework foundation"]
-   [:url "https://github.com/donut-power/datapotato"]
+   [:url "https://github.com/donut-party/datapotato"]
    [:licenses
     [:license
      [:name "MIT"]
@@ -28,9 +28,9 @@
     [:developer
      [:name "Daniel Higginbotham"]]]
    [:scm
-    [:url "https://github.com/donut-power/frontend"]
-    [:connection "scm:git:https://github.com/donut-power/frontend.git"]
-    [:developerConnection "scm:git:ssh:git@github.com:donut-power/frontend.git"]
+    [:url "https://github.com/donut-party/frontend"]
+    [:connection "scm:git:https://github.com/donut-party/frontend.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:donut-party/frontend.git"]
     [:tag (str "v" version)]]])
 
 (defn- jar-opts [opts]
@@ -58,3 +58,9 @@
     (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
                 :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
   opts)
+
+(defn install [opts]
+  (let [opts (jar-opts opts)]
+    (jar opts)
+    (b/install opts)
+    (println version)))
